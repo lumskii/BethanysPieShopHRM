00d1d09697f91c6bfe01f6aa39e5c995b608df51
lumskii <67390011+lumskii@users.noreply.github.com>
2024-06-19 23:34:00 -0700

    first method

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// wage calculator
+
+static int calculatorYearlyWage(int monthlyWage, int numOfMonthsWorked){
+    //Console.WriteLine($"Yearly wage: {monthlyWage * numOfMonthsWorked}");
+    return monthlyWage * numOfMonthsWorked;
+}
+
+int amount = 1234;
+int months = 12;
+
+// Console.WriteLine(calculatorYearlyWage(amount, months));
+calculatorYearlyWage(amount, months);
