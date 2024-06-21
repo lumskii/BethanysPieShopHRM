40c38e7ad0a90c5c91a020e7b8ede404994cdb0a
lumskii <67390011+lumskii@users.noreply.github.com>
2024-06-21 08:44:00 -0700

    chore: Update calculatorYearlyWage function to handle different number of months worked

--- Program.cs
+++ Program.cs
@@ -2,11 +2,21 @@
 
 static int calculatorYearlyWage(int monthlyWage, int numOfMonthsWorked){
     //Console.WriteLine($"Yearly wage: {monthlyWage * numOfMonthsWorked}");
-    return monthlyWage * numOfMonthsWorked;
+    // return monthlyWage * numOfMonthsWorked;
+    
+    if(numOfMonthsWorked == 12){
+        return monthlyWage * (numOfMonthsWorked + 1);
+    } else {
+        return monthlyWage * numOfMonthsWorked;
+    }
 }
 
 int amount = 1234;
-int months = 12;
+int months = 11;
 
 // Console.WriteLine(calculatorYearlyWage(amount, months));
-calculatorYearlyWage(amount, months);
+int yearlyWage = calculatorYearlyWage(amount, months);
+
+Console.WriteLine($"Yearly wage: {yearlyWage}");
+
+Console.ReadLine();
